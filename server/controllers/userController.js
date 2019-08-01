6b0c688d884f76c5c3e5358bdd4184bc5981fa0a
unknown <jazzonahimana@gmail.com>
2019-08-01 16:50:17 +0200

    feature(signin): User should be able to signin [Finishes #167417973]

---
+++ server/controllers/userController.js
@@ -0,0 +1,25 @@
+import {jwtAuthorization} from '../helpers/authorization';
+ import  jwt  from 'jsonwebtoken';
+import  users  from '../data/users';
+import {validateUserSignup, validateUserSignin } from '../helpers/DataValidation';
+
+const usersController = {
+   
+    userSignin: (req, res) => {
+      const token = jwtAuthorization(req, res);
+      const { error } = validateUserSignin(req.body);
+      if(error) return res.status(400).send(error.details[0].message);       
+      const signedUser = users.find(user => user.email === req.body.email && user.password === req.body.password);
+             
+      if(!signedUser) return res.status(400).send('Invalid user credentials...');
+      
+      signedUser.token = token;
+      return res.status(200).json({
+       status: res.statusCode,
+       status:'success',
+             data:signedUser,
+     });
+  }     
+  };
+  export default usersController;
+
---
+++ server/data/users.js
@@ -0,0 +1,19 @@
+// export const users =  [
+const users =  [
+    {
+        
+        email:'jazzonahimana@gmail.com',
+        password:'test123',
+        first_name:'janvier',
+        last_name:'nahimana'
+    },
+    {
+        
+        email:'jazzonahimana1@gmail.com',
+        password:'test',
+        first_name:'janvier',
+        last_name:'nahimana'
+    }
+];
+// module.exports = users;
+export default users; 
