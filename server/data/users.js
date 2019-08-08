6079f7d219c695c5ace02bcb0bc12e8a2b21fd52
unknown <jazzonahimana@gmail.com>
2019-08-08 15:57:01 +0200

    feature(signin): User should signup [finishes #167785438]

--- server/data/users.js
+++ server/data/users.js
@@ -1,19 +1,20 @@
 // export const users =  [
-const users =  [
-    {
-        
-        email:'jazzonahimana@gmail.com',
-        password:'test123',
-        first_name:'janvier',
-        last_name:'nahimana'
-    },
-    {
-        
-        email:'jazzonahimana1@gmail.com',
-        password:'test',
-        first_name:'janvier',
-        last_name:'nahimana'
-    }
+const users = [
+  {
+    email: 'jazzonahimana@gmail.com',
+    password: 'test123',
+    first_name: 'janvier',
+    last_name: 'nahimana',
+    isAdmin: false
+  },
+  {
+
+    email: 'jazzonahimana1@gmail.com',
+    password: 'test123',
+    first_name: 'janvier',
+    last_name: 'nahimana',
+    isAdmin: true
+  }
 ];
 // module.exports = users;
-export default users; 
+export default users;
