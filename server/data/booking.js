f91d8c60e627653637831a29cfec938bb816f410
unknown <jazzonahimana@gmail.com>
2019-08-08 17:09:43 +0200

    feature(book seat): As a user I should be able to book a seat [finishes #167700290]

---
+++ server/data/booking.js
@@ -0,0 +1,12 @@
+const bookings = [
+    {
+        booking_id:1,
+        bus_licence_number:"RAD2019",
+        trip_date:"2019-12-32",
+        first_name:"nahimana",
+        last_name:"janvier",
+        user_email:"jazzonahimana"
+        
+    }];  
+// module.exports = bookings;
+export default bookings;
